f03fa36aade4cd89adef3c3716bca4cafdf01a33
dhruvramani <dhruvramani98@gmail.com>
2018-05-12 10:33:38 +0300

    trying w/o iterators - trying

--- dataset.py
+++ dataset.py
@@ -40,7 +40,7 @@ class SST_Data():
 
         #self.train_iter, self.val_iter, self.test_iter = datasets.SST.iters(batch_size=4)
 
-        print('train[0:batch_size)', [vars(self.train[i]) for i in range(4)])
+        print('train[0:batch_size)', [vars(self.train[i]) for i in range(1, 10)])
 
         #batch = next(iter(self.train_iter))
         #print(batch.text)
