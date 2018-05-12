5fe9168b7fc99b8ba14e4b15b9d0745f6ce41582
dhruvramani <dhruvramani98@gmail.com>
2018-05-12 09:42:22 +0300

    vector 1

--- dataset.py
+++ dataset.py
@@ -27,8 +27,13 @@ class SST_Data():
 
         print('len(train)', len(self.train))
 
-        self.TEXT.build_vocab(self.train, vectors=[GloVe(name='840B', dim='300'), CharNGram(), FastText()])
-        self.LABEL.build_vocab(self.train)
+        f = FastText()
+        self.TEXT.build_vocab(train, vectors=f)
+        self.TEXT.vocab.extend(f)
+        self.LABEL.build_vocab(train)
+        
+        #self.TEXT.build_vocab(self.train, vectors=[GloVe(name='840B', dim='300'), CharNGram(), FastText()])
+        #self.LABEL.build_vocab(self.train)
 
         print('len(TEXT.vocab)', len(self.TEXT.vocab))
         print('TEXT.vocab.vectors.size()', self.TEXT.vocab.vectors.size())
