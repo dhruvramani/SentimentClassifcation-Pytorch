3d0abb7ea8c5e9614c8d31c5a3766905b850c3c6
dhruvramani <dhruvramani98@gmail.com>
2018-05-12 11:32:27 +0530

    diff vocab approach

--- dataset.py
+++ dataset.py
@@ -27,15 +27,17 @@ class SST_Data():
 
         print('len(train)', len(self.train))
 
-        self.url = 'https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.simple.vec'
-        self.TEXT.build_vocab(self.train, vectors=Vectors('wiki.simple.vec', url=self.url))
+        #self.url = 'https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.simple.vec'
+        #self.TEXT.build_vocab(self.train, vectors=Vectors('wiki.simple.vec', url=self.url))
+        #self.LABEL.build_vocab(self.train)
+
+        self.TEXT.build_vocab(self.train, vectors=[GloVe(name='840B', dim='300'), CharNGram(), FastText()])
         self.LABEL.build_vocab(self.train)
 
         print('len(TEXT.vocab)', len(self.TEXT.vocab))
         print('TEXT.vocab.vectors.size()', self.TEXT.vocab.vectors.size())
 
-        self.train_iter, self.val_iter, self.test_iter = data.BucketIterator.splits(
-            (self.train, self.val, self.test), batch_size=3, device=0)
+        train_iter, val_iter, test_iter = datasets.SST.iters(batch_size=4)
 
         batch = next(iter(self.train_iter))
         print(batch.text)
